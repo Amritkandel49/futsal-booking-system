c7242462abab44c74119b49b087e6aab79cd28c1
Amritkandel49 <amritkandel062@gmail.com>
2024-08-04 20:58:29 +0545

    Fixed some backend errors

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,10 +25,14 @@ app.use(express.static('public'));
 import ownerRouter from './routes/owner.route.js';
 import trufRouter from './routes/turf.route.js';
 import userRouter from './routes/user.route.js';
+import bookingRouter from './routes/booking.route.js';
+
 
 
 app.use('/api/v1/owners', ownerRouter);
 app.use('/api/v1/turfs', trufRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/bookings', bookingRouter);
+
 
 export default app;
---
+++ backend/src/routes/booking.route.js
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+
+import {
+    createBooking,
+    getUserBookings,
+    getTurfBookings,
+    cancelBooking
+} from "../controllers/booking.controller.js";
+
+const router = Router();
+
+//router for Creating Booking
+
+router.post('/', createBooking);
+
+//router for getting bookings for a specific turf
+
+router.get('/turf/:turf_id', getTurfBookings);
+
+//router for getting bookings for a specific user
+
+router.get('/user/:user_id', getUserBookings);
+
+//router for cancelling bookings
+
+router.patch('/cancel/:booking_id', cancelBooking); //patch is used because we only have to update the status field to mark it as cancelled
+
+export default router;
